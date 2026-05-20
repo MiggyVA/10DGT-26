aab2c7bac0584699c91f63adf3d0e511449788f8
MiggyVA <miguel.agustin@kingsway.school.nz>
2026-05-21 09:27:43 +1200

    My first program in Python named Print

---
+++ print.py
@@ -0,0 +1,23 @@
+greeting = "Hello"
+print(greeting)
+
+# types of strings
+string_eg1 = "This is a string." # A string contains speeech marks
+string_eg2 = "7"
+
+# Additional example
+num_1 = "7" #String
+num_2 = "5"
+
+sum = num_1 + num_2 # Concatenation. Adding words together
+print(sum)
+
+num_1 = 7.5 #Float
+num_2 = 5 #Integer
+
+sum = num_1 + num_2 # Addition of numbers
+print(sum)
+
+# Cofee program. Checking whether you like coffee
+like_coffee = input("Do you like coffee?")
+print("like_coffee")
